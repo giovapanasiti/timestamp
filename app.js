8da2ba1fe4e30537cacad529741c6eba0accc0e2
Giovanni Panasiti <g.panasiti@enpam.it>
2017-07-05 16:58:06 +0200

    convert date value

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ var app = module.exports = express();
 app.use(bodyParser.json());
 app.use(cors());
 
+// GET Call return a json that formats dates
+app.get('/date/:dateParam', function(req, res, next){
+  console.log('URL Works');
+  // read the param
+  var dateValue = req.params.dateParam;
+  // options for formatting date in natural date view
+  var dateFormatOpt = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric'
+  };
+
+  if (isNaN(dateValue)){
+    var naturalDate = new Date(dateValue)
+    naturalDate = naturalDate.toLocaleDateString("en-US", dateFormatOpt);
+
+    var unixDate = new Date(dateValue).getTime()/1000;
+  }
+
+  // return dateValue;
+  res.json({unix: unixDate, natural: naturalDate});
+})
+
 app.listen(4500, function(){
   console.log('aaaa');
 })
